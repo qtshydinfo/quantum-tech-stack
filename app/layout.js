1b67efbd1c86079135b778751bddbf58e8fd2dc0
Quantum Tech Stack <qtshyd.info@gmail.com>
2026-02-20 17:52:53 +0530

    Added metadata for title and favicon

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,19 @@
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
-
 import "./globals.css";
+
+export const metadata = {
+  title: {
+    default: "Quantum Tech Stack",
+    template: "%s | Quantum Tech Stack",
+  },
+  description:
+    "Quantum Tech Stack is a modern MERN stack development company building scalable SaaS platforms and enterprise applications.",
+  icons: {
+    icon: "/icon.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +21,6 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         <Toaster position="top-right" />
-
         {children}
         <Footer />
       </body>
